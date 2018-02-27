36c5d4ff86796f82c48922ebe9360cdf0ec63859
erdmann-casey <31048811+erdmann-casey@users.noreply.github.com>
2018-02-27 17:15:20 -0500

    test api call for referencing the seatgeekapi

---
+++ services/test-api.php
@@ -0,0 +1,25 @@
+<?php
+
+
+
+$api_key = "MTA0NTE3MTN8MTUxNzUyMDU4OC41Mw";
+
+$event_id = $_REQUEST['id'];
+
+$url = "https://api.seatgeek.com/2/events?client_id=". $api_key;
+
+if(isset($event_id)){
+    $url .= "&id=" . $event_id;
+}
+
+
+
+//var_dump($url);
+
+$data = file_get_contents($url);
+
+
+echo $data;
+
+
+?>
