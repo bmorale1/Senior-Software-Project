2889a8269508009504816980c68a15e100c54529
Injection <cerdmann@injecti0n.org>
2018-03-14 15:29:27 -0400

    Fix forms and add main site

--- services/reset-password.php
+++ services/reset-password.php
@@ -61,6 +61,7 @@ if(isset($_GET['id']) && isset($_GET['code'])){
     <link href="../bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen">
     <link href="../bootstrap/css/bootstrap-responsive.min.css" rel="stylesheet" media="screen">
     <link href="../assets/styles.css" rel="stylesheet" media="screen">
+	<link rel="Stylesheet" type = "text/css" href="ticketGeek.css">
      <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
     <!--[if lt IE 9]>
       <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
