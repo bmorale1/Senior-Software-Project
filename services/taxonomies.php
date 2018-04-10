21031a7235c3e8a3372e4b5e30189670a38873f3
Cory Yelverton <yelverton_cory@columbusstate.edu>
2018-04-10 17:19:58 -0400

    Fixed Period Bug

--- services/taxonomies.php
+++ services/taxonomies.php
@@ -1,18 +1,18 @@
 <?php
-
+header('Content-type: application/json');
 
 $api_key = "MTA0NTE3MTN8MTUxNzUyMDU4OC41Mw";
 
-$taxonomies_name = $_REQUEST['taxonomies.name'];
-$taxonomies_id = $_REQUEST['taxonomies.id'];
+$taxonomies_name = $_REQUEST['taxonomies_name'];
+$taxonomies_id = $_REQUEST['taxonomies_id'];
 
 $url = "https://api.seatgeek.com/2/taxonomies?client_id=". $api_key;
 
 if(isset($taxonomies_name)){
-    $url .= "&taxonomies.name=" . $taxonomies_name;
+    $url .= "&taxonomies_name=" . $taxonomies_name;
 }
 if(isset($taxonomies_id)){
-    $url .= "&taxonomies.id=" . $taxonomies_id;
+    $url .= "&taxonomies_id=" . $taxonomies_id;
 }
 
 
